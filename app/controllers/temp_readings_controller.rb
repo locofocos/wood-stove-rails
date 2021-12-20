3f82fc8b215ca311f43d800f928060ee0d920d27
Patrick Boatner <patrickboatner@gmail.com>
2021-12-20 12:46:53 -0600

    Temp readings index

--- app/controllers/temp_readings_controller.rb
+++ app/controllers/temp_readings_controller.rb
@@ -1,4 +1,5 @@
 class TempReadingsController < ApplicationController
   def index
+    @temp_readings = TempReading.order(created_at: :desc).limit(20)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'temp_monitors/show'
   get 'temp_monitors/edit'
   get 'temp_monitors/update'
+
   get 'temp_readings/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
