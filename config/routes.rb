be152c2793c4eee88523c75d8c79cff215b79895
Patrick Boatner <patrickboatner@gmail.com>
2021-12-21 00:53:57 -0600

    all the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :temp_readings, only: [:index]
-  resources :temp_monitors, only: [:index, :show, :edit, :update]
+  resources :temp_monitors
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
