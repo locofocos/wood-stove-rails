c2be92f1617b4a4fe83dd1fba77fd456e5b34b41
Patrick Boatner <patrickboatner@gmail.com>
2021-12-27 21:35:27 -0600

    fix when param is missing

--- app/controllers/temp_readings_controller.rb
+++ app/controllers/temp_readings_controller.rb
@@ -1,6 +1,6 @@
 class TempReadingsController < ApplicationController
   def index
-    limit = params[:limit].to_i || 30
+    limit = params[:limit].present? ? params[:limit].to_i : 30
     @temp_readings = TempReading.order(created_at: :desc).limit(limit)
   end
 end
