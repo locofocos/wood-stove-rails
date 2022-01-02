e8632cf1c02a2a89f1202d2f46a3c5b9aa298313
Patrick Boatner <patrickboatner@gmail.com>
2022-01-01 23:11:36 -0600

    Display monitors on chart

--- app/controllers/temp_readings_controller.rb
+++ app/controllers/temp_readings_controller.rb
@@ -1,6 +1,25 @@
+# frozen_string_literal: true
+
 class TempReadingsController < ApplicationController
   def index
     limit = params[:limit].present? ? params[:limit].to_i : 30
     @temp_readings = TempReading.order(created_at: :desc).limit(limit)
+
+    @monitors_for_chart = []
+    TempMonitor.all.each do |monitor|
+      if monitor.upper_limitf
+        name = "Monitor #{monitor.id} upper"
+        monitor.upper_limitf
+        color = 'red'
+        @monitors_for_chart << { name: name, value_f: monitor.upper_limitf, color: color }
+      end
+
+      if monitor.lower_limitf
+        name = "Monitor #{monitor.id} lower"
+        monitor.lower_limitf
+        color = 'blue'
+        @monitors_for_chart << { name: name, value_f: monitor.lower_limitf, color: color }
+      end
+    end
   end
 end
