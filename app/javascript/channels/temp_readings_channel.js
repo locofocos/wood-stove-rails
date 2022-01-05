a0cf32a82de79611acc7f38497397aa6df058e93
Patrick Boatner <patrickboatner@gmail.com>
2022-01-04 18:42:24 -0600

    Only auto refresh on temp readings page

--- app/javascript/channels/temp_readings_channel.js
+++ app/javascript/channels/temp_readings_channel.js
@@ -13,6 +13,9 @@ consumer.subscriptions.create("TempReadingsChannel", {
   received(data) {
     // Called when there's incoming data on the websocket for this channel
     console.log("Received TempReadingsChannel data! - " + data)
-    location.reload(); // just reload the page, no need to do anything fancy. Plus there are some server-rendered charts and tables, just keep it simple.
+
+    if (window.location.pathname === '/temp_readings') { // only refresh on this page because the settings/monitors pages don't need this and it would disrupt editing settings/monitors
+      location.reload(); // just reload the page, no need to do anything fancy. Plus there are some server-rendered charts and tables, just keep it simple.
+    }
   }
 });
