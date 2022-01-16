2315ad6ae46b6a3fc6f064356e58045420cdb636
Patrick Boatner <patrickboatner@gmail.com>
2022-01-15 23:17:26 -0600

    Fix bug, refresh was broken on homepage chart link

--- app/javascript/channels/temp_readings_channel.js
+++ app/javascript/channels/temp_readings_channel.js
@@ -14,7 +14,7 @@ consumer.subscriptions.create("TempReadingsChannel", {
     // Called when there's incoming data on the websocket for this channel
     console.log("Received TempReadingsChannel data! - " + data)
 
-    if (window.location.pathname === '/temp_readings') { // only refresh on this page because the settings/monitors pages don't need this and it would disrupt editing settings/monitors
+    if (window.location.pathname === '/temp_readings' || window.location.pathname === '/') { // only refresh on this page because the settings/monitors pages don't need this and it would disrupt editing settings/monitors
       location.reload(); // just reload the page, no need to do anything fancy. Plus there are some server-rendered charts and tables, just keep it simple.
     }
   }
